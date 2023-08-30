05750d8cf59bbd6953fdc725fc6dfcca794e7b91
Amelie Ploil <amelie@ploil.com>
2023-08-30 14:37:31 +0200

    d

--- .github/workflows/checkissues.ts
+++ .github/workflows/checkissues.ts
@@ -15,31 +15,3 @@ export function identify(s: string) {
     return "unsure";
   }
 }
-
-function checkUnsure(s: string) {
-  if (s == "unsure") {
-    //Title was wrong, close / delete after
-    return "Something with you title seems to be wrong, check if the right emoji is at the beginning of your title";
-  }
-  console.log("Check:" + s);
-}
-
-function checkQuestion(s) {
-  if (s == "question") {
-    //TODO: check for valid information
-  }
-}
-
-function checkBug(s) {
-  if (s == "bug") {
-    //TODO: check for valid information
-  }
-  console.log("Check:" + s);
-}
-
-function checkFeature(s) {
-  if (s == "bug") {
-    //TODO: check for valid information
-  }
-  console.log("Check:" + s);
-}
