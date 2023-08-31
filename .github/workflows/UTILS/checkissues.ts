0a85726cbb530fddd734ba66d372b7d7b8a2d667
Amelie Ploil <amelie@ploil.com>
2023-08-31 15:42:54 +0200

    emr

--- .github/workflows/UTILS/checkissues.ts
+++ .github/workflows/UTILS/checkissues.ts
@@ -23,38 +23,34 @@ export function identify(title: string) {
 }
 
 function cleanTitle(title: string, emoji: string) {
+  let emr = "";
   if (title.includes("emoji") && title.indexOf("emoji") > 0) {
-    localerrorAdd("!Title: Emoji has to be at the beginning of your Title \n");
-    return "w";
+    // localerrorAdd("!Title: Emoji has to be at the beginning of your Title \n");
+    emr += "!Title: Emoji has to be at the beginning of your Title \n";
   } else if (title.length < 5) {
-    localerrorAdd("!Title: Title is to short \n");
-    console.log("AHAHAHH: " + errorTitle);
-    return "w";
-  } else return "w";
+    //   localerrorAdd("!Title: Title is to short \n");
+    emr += "!Title: Title is to short \n";
+  }
+  return emr;
 }
 
-export function cleanBody(
-  title: string,
-  body: string,
-  emoji: string,
-  error: string
-) {
+export function cleanBody(title: string, body: string, emoji: string) {
   const cT = cleanTitle(title, emoji);
-  if (cT == "w") {
-    const parts = body.split("###");
+  console.log("EMR=" + cT);
 
-    parts.forEach((s) => {
-      try {
-        const twPart = s.split("\n");
-        const key = twPart[0];
-        const value = twPart.slice(1).join("\n");
-        const valuec = value.replace(/`/g, "");
-        const cleanvalue = valuec.replace(/\n/g, " ");
+  const parts = body.split("###");
 
-        labels[key] = cleanvalue;
-      } catch (error) {}
-    });
-  }
+  parts.forEach((s) => {
+    try {
+      const twPart = s.split("\n");
+      const key = twPart[0];
+      const value = twPart.slice(1).join("\n");
+      const valuec = value.replace(/`/g, "");
+      const cleanvalue = valuec.replace(/\n/g, " ");
+
+      labels[key] = cleanvalue;
+    } catch (error) {}
+  });
 }
 
 function localerrorAdd(value: string) {
