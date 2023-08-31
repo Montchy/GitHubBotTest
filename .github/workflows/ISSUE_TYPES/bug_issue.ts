e16da660cef6a9e9f8cf0466e1b37563b628492b
Amelie Ploil <amelie@ploil.com>
2023-08-31 13:05:30 +0200

    ss

--- .github/workflows/ISSUE_TYPES/bug_issue.ts
+++ .github/workflows/ISSUE_TYPES/bug_issue.ts
@@ -1,6 +1,6 @@
 import { isSemVer } from "../UTILS/helpingMethods";
 import { cleanBody, labels, errorTitle } from "../UTILS/checkissues";
-import { errorAddTitle, errorAdd } from "../UTILS/helpingMethods";
+import { errorAddTitle } from "../UTILS/helpingMethods";
 
 let error = "";
 
@@ -23,34 +23,28 @@ function cleanup(key: string) {
     let value = labels[key];
     if (value != "" && value != null && value.length >= 5) {
     } else
-      errorAdd(
-        "!What were you trying to do: Empty or less than 5 letters\n",
-        error
-      );
+      errorAdd("!What were you trying to do: Empty or less than 5 letters\n");
   }
   if (key == " Reproduceable Code") {
     let value = labels[key];
 
     if (value != "" && value != null && value.length >= 14) {
-    } else
-      errorAdd("!Reproducable Code: Empty or less than 5 letters\n", error);
+    } else errorAdd("!Reproducable Code: Empty or less than 5 letters\n");
   }
   if (key == " What happened instead?") {
     let value = labels[key];
     if (value != "" && value != null && value.length >= 5) {
-    } else
-      errorAdd("!What happened instead: Empty or less than 5 letters\n", error);
+    } else errorAdd("!What happened instead: Empty or less than 5 letters\n");
   }
   if (key == " Relevant log output") {
     let value = labels[key];
     if (value != "" && value != null && value.length >= 16) {
-    } else
-      errorAdd("!Relevant log output: Empty or less than 5 letters\n", error);
+    } else errorAdd("!Relevant log output: Empty or less than 5 letters\n");
   }
   if (key == " Device") {
     let value = labels[key];
     if (value != "" && value != null && value.length >= 5) {
-    } else errorAdd("!Device: Empty or less than 5 letters\n", error);
+    } else errorAdd("!Device: Empty or less than 5 letters\n");
   }
   if (key == " VisionCamera Version") {
     let value = labels[key];
@@ -58,9 +52,17 @@ function cleanup(key: string) {
 
     if (isSemVer(cleanvalue) == true) {
       return null;
-    } else errorAdd("!VisionCamera Version: Isn't SemVer!\n", error);
+    } else errorAdd("!VisionCamera Version: Isn't SemVer!\n");
   }
   return "Fatal error";
+}
+
+function errorAdd(value: string) {
+  if (error == "") {
+    error = "Errors in the Body: \n" + value;
+  } else {
+    error = error + value;
+  }
 }
 
 export function returnErrorBug() {
