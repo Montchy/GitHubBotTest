f97eb54c400c859d489f50d0392f64c6c4d14f9a
Amelie Ploil <amelie@ploil.com>
2023-08-31 11:07:50 +0200

    sf

--- .github/workflows/feature_issue.ts
+++ .github/workflows/feature_issue.ts
@@ -39,7 +39,30 @@ export function cleanBodyFeature(title: string, body: string) {
   } else return "l";
 }
 
-function cleanup(key: string) {}
+function cleanup(key: string) {
+  if (key == " What feature or enhancement are you suggesting?") {
+    let value = labels[key];
+    if (value != "" && value != null && value.length >= 5) {
+    } else
+      errorAdd(
+        "!What feature or enhancement are you suggesting?: Empty or less than 5 letters\n"
+      );
+  }
+  if (key == " What Platforms whould this feature/enhancement affect?") {
+    let value = labels[key];
+    if (value != null) {
+    } else
+      errorAdd(
+        "!What Platforms whould this feature/enhancement affect?: cant be empty"
+      );
+  }
+  if (key == "  Alternatives/Workarounds") {
+    let value = labels[key];
+    if (value != "" && value != null && value.length >= 5) {
+    } else
+      errorAdd("!Alternatives/Workarounds: Empty or less than 5 letters\n");
+  }
+}
 
 function errorAdd(value: string) {
   if (error == "") {
